ab8fe789767cbfe20a5466338e0272164216f8d4
Benjamin Simmons <bensimmons325@gmail.com>
2018-10-31 10:43:20 -0400

    Created project in VS

---
+++ CapstoneProject/CapstoneProject/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CapstoneProject.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CapstoneProject/CapstoneProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapstoneProject.Startup))]
+namespace CapstoneProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
